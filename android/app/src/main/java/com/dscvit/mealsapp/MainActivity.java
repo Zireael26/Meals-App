1c304ed4cf6de876475ff127363b0f9c618cec80
Abhishek Kaushik <abhishek.nexus26@gmail.com>
2020-03-28 16:16:47 +0530

    Add some dummy data and make basic grid
    Signed-off-by: Abhishek Kaushik <abhishek.nexus26@gmail.com>

---
+++ android/app/src/main/java/com/dscvit/mealsapp/MainActivity.java
@@ -0,0 +1,6 @@
+package com.dscvit.mealsapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/pages/categories_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:mealsapp/dummay_data.dart';
+import 'package:mealsapp/widgets/category_item.dart';
+
+class CategoriesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Deli Meals'),
+      ),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200.0,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(
+                  catData.title,
+                  catData.color,
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
