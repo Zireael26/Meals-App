cc2609d4da59a3c17dcedeeaf624051cdbf1fd54
Abhishek Kaushik <abhishek.nexus26@gmail.com>
2020-05-04 05:56:49 +0530

    Added Bottom Tab Version
    Signed-off-by: Abhishek Kaushik <abhishek.nexus26@gmail.com>

--- lib/pages/tabs_page.dart
+++ lib/pages/tabs_page.dart
@@ -8,32 +8,44 @@ class TabsPage extends StatefulWidget {
 }
 
 class _TabsPageState extends State<TabsPage> {
+  final List<Map<String, dynamic>> _pages = [
+    {'page': CategoriesPage(), 'title': "Categories"},
+    {'page': FavoritesPage(), 'title': "Favorites"},
+  ];
+
+  int _selectedPageIndex = 0;
+
+  _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Meals"),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: "Categories",
-              ),
-              Tab(
-                icon: Icon(Icons.favorite),
-                text: "Favorites",
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Theme
+            .of(context)
+            .primaryColor,
+        currentIndex: _selectedPageIndex,
+//        type: BottomNavigationBarType.shifting,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text("Categories"),
           ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesPage(),
-            FavoritesPage(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            title: Text("Favorites"),
+          ),
+        ],
       ),
     );
   }
